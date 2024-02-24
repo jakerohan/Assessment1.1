905f3997996d5f4489e66e166f9b314c97adce3c
Jake Rohan <jake.rohan@gmail.com>
2024-02-24 18:13:44 +1030

    Adding the base for the JS portion of the assignment

---
+++ JS_CSS/js.js
@@ -0,0 +1,25 @@
+
+function changeimage1(){
+    var img = document.getElementById("image");
+    img.src="http://www.outgrabe.net/bird00.jpg";
+    }
+
+function changeimage2(){
+    var img = document.getElementById("image");
+    img.src="http://www.outgrabe.net/bird01.jpg";
+    }
+
+function changeimage3(){
+    var img = document.getElementById("image");
+    img.src="http://www.outgrabe.net/bird02.jpg";
+    }
+
+function changeimage4(){
+    var img = document.getElementById("image");
+    img.src="http://www.outgrabe.net/bird03.jpg";
+    }
+
+function changeimage5(){
+    var img = document.getElementById("image");
+    img.src="http://www.outgrabe.net/bird04.jpg";
+    }
