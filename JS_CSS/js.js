b1b2f9e5a2b7b958a3a04d5c61349e36c31a2994
Jake Rohan <jake.rohan@gmail.com>
2024-02-24 18:26:06 +1030

    adding captions

--- JS_CSS/js.js
+++ JS_CSS/js.js
@@ -2,24 +2,29 @@
 function changeimage1(){
     var img = document.getElementById("image");
     img.src="http://www.outgrabe.net/bird00.jpg";
+    document.getElementById("caption").innerHTML ="Pardalote by fir0002 (CC-by-NC)";
     }
 
 function changeimage2(){
     var img = document.getElementById("image");
     img.src="http://www.outgrabe.net/bird01.jpg";
-    }
+    document.getElementById("caption").innerHTML ="Purple swamp hen by Toby Hudson (CC-by-SA)";
+}
 
 function changeimage3(){
     var img = document.getElementById("image");
     img.src="http://www.outgrabe.net/bird02.jpg";
-    }
+    document.getElementById("caption").innerHTML ="White-headed Stilt by JJ Harrison (CC-by-SA)";
+}
 
 function changeimage4(){
     var img = document.getElementById("image");
     img.src="http://www.outgrabe.net/bird03.jpg";
-    }
+    document.getElementById("caption").innerHTML ="Inland Thornbill by Peter Jacobs (CC-by-SA)";
+}
 
 function changeimage5(){
     var img = document.getElementById("image");
     img.src="http://www.outgrabe.net/bird04.jpg";
-    }
+    document.getElementById("caption").innerHTML ="Rose Robin by JJ Harrison (CC-by-SA)";
+}
